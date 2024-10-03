6501e861a2240ebfd9fc9eb6d5256cab434cbc7d
Suneel-Ahmed <suneelahmed413@gmail.com>
2024-10-04 00:50:17 +0500

    strapi_test_TASK

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          'localhost', // Your local development domain
+          'supportive-car-b7a635f0ab.media.strapiapp.com' // Add your external image domain
+        ],
+      },
+   
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,77 @@
-import Image from "next/image";
+"use client"
+import React , {useState , useEffect} from 'react'
+import Image from 'next/image'
+import img1 from './images/1.jpg'
+import { IoIosArrowBack } from "react-icons/io";
 
-export default function Home() {
+const  page = () => {
+const [data , setData] = useState()
+const [lists , setLists] = useState()
+  
+
+  const handleList = (value)=>{
+    setData(value)
+   
+  }
+
+
+  const FetchApi = async ()=>{
+    const Apidata = await fetch(`https://supportive-car-b7a635f0ab.strapiapp.com/api/choose-uses?populate=*`)
+    let posts = await Apidata.json()
+    setLists(posts)
+  }
+
+
+  useEffect(()=>{
+ 
+    FetchApi()
+    
+  },[])
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className='w-full h-screen flex-col px-[200px]  flex  items-center bg-white' >
+        <div className='flex flex-col items-center w-[800px] mt-[100px] text-center gap-[10px]' >
+              <h1 className='text-[20px]' >Why Choose Us</h1>
+              <h1 className='text-[26px] font-semibold' >We Are Different From Others</h1>
+              <h1 className='text-[18px] font-normal' >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className='flex  mt-[30px]  w-full items-center  ' >
+              <div className='relative h-[400px] min-h-fit w-[70%]' >
+                <div className='w-[400px] absolute top-0 left-[200px] h-[400px] flex flex-col justify-center items-center rounded-full bg-yellow-400' >
+           
+              <Image width={500} height={500} priority alt='image' src={ !data ? img1 : `${data?.image?.url}`  } className='w-full h-full rounded-full object-cover object-center' />                
+
+                </div>
+                <div className='w-[400px] text-white flex flex-col justify-center items-start p-[30px] absolute top-0 left-0 h-[400px] rounded-full bg-[#CB4263]/90' >
+                <h1 className='text-[18px] font-semibold' >{data?.title ||  lists?.data[0].title  }</h1>
+                <p>
+                {data?.para ||  lists?.data[0].para  }
+                </p>
+                </div>
+              </div>
+              <div className='w-[30%] flex justify-end '>
+                <ul className='flex flex-col gap-[10px]' >
+                  {
+                    lists?.data?.map((val)=>(
+                   <li 
+                   key={val.id}
+                   onClick={()=>handleList(val)}
+                  className='w-[300px] py-[5px] cursor-pointer bg-red-500 text-white rounded-s-full flex items-center font-semibold text-[16px] justify-between px-[10px]'
+                  >
+                    <IoIosArrowBack/>
+                    {val.title}
+                  
+                  </li>
+                    ))
+                  }              
+                </ul>
+              </div>
+        </div>
+        
+    </main>
+  )
 }
+
+export default page
+
+
